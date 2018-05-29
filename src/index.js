12d4ffc4ab6815a5d274d2c5d8f178a3529eb355
Stanislau Matsievsky <st1ch@Stanislaus-MacBook-Pro.local>
2018-05-29 21:19:15 +0300

    init webpack files and modified gitignore

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import style from "./main.css";
+
+console.log("I'n a silly entry point");
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
