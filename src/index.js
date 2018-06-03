581eff31af9133b39e9223fdb4fe03297c57e8bc
Stanislau Matsievsky <st1ch@Stanislaus-MacBook-Pro.local>
2018-06-03 22:11:15 +0300

    add jquery, fullpage.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 
 import style from "./main.css";
 import $ from 'jquery';
+import fullPage from 'fullPage.js/dist/jquery.fullPage.js'
+import fullPageCss from 'fullPage.js/dist/jquery.fullPage.css'
 
 
-
-
-
-
-  $(".js-scroll-to").click(function(e) {
-    let destination = $(this).attr('href');
-    e.preventDefault();
-
-    // Animate scroll to destination
-    $('html, body').animate({
-      scrollTop: $(destination).offset().top
-    }, 1000);
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
   });
+});
+
+
+
+  // $(".js-scroll-to").click(function(e) {
+  //   let destination = $(this).attr('href');
+  //   e.preventDefault();
+  //
+  //   // Animate scroll to destination
+  //   $('html, body').animate({
+  //     scrollTop: $(destination).offset().top
+  //   }, 1000);
+  // });
